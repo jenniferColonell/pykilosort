Add ks2.0 functionality, under control of ks2_mode parameter Fix bug in average_snips in cluster.py If both fshigh and fslow are NOT set, filtering is skipped completely

pykilosort/cluster.py
@@ -222,10 +222,11 @@ def initializeWdata2(call, uprojDAT, Nchan, nPCs, Nfilt, iC):
     # pick random spikes from the sample
     # WARNING: replace ceil by floor because this is a random index, and 0/1 indexing
     # discrepancy between Python and MATLAB.
-    irand = np.floor(np.random.rand(Nfilt) * uprojDAT.shape[1]).astype(np.int32)
-
+    # intialize random number generator with CSBSeed
+    
+    irand = np.floor(np.random.rand(Nfilt) * uprojDAT.shape[1]).astype(np.int32) 
     W = cp.zeros((nPCs, Nchan, Nfilt), dtype=np.float32)
-
+    
     for t in range(Nfilt):
         ich = iC[:, call[irand[t]]]  # the channels on which this spike lives
         # for each selected spike, get its features
@@ -375,10 +376,12 @@ def mexClustering2(Params, uproj, W, mu, call, iMatch, iC):
     bestFilter = cp.RawKernel(code, 'bestFilter')
     bestFilter((40,), (256,), (d_Params, d_iMatch, d_iC, d_call, d_cmax, d_id, d_x))
 
-    # average all spikes for same template -- ORIGINAL
+    # average all spikes for same template -- threaded over filters, but not features
+    # avoid collision when adding to elements of d_dWU
     average_snips_v2 = cp.RawKernel(code, 'average_snips_v2')
     average_snips_v2(
-        (Nfilters,), (NrankPC, NchanNear), (d_Params, d_iC, d_call, d_id, d_uproj, d_cmax, d_dWU))
+        (Nfilters,), (1,), (d_Params, d_iC, d_call, d_id, d_uproj, d_cmax, d_dWU))
+    #    (Nfilters,), (NrankPC, NchanNear), (d_Params, d_iC, d_call, d_id, d_uproj, d_cmax, d_dWU))
 
     count_spikes = cp.RawKernel(code, 'count_spikes')
     count_spikes((7,), (256,), (d_Params, d_id, d_nsp, d_x, d_V))
@@ -434,7 +437,6 @@ def clusterSingleBatches(ctx, sanity_plots=False, plot_widgets=None, plot_pos=0)
     Nbatch = ctx.intermediate.Nbatch
     params = ctx.params
     probe = ctx.probe
-    raw_data = ctx.raw_data
     ir = ctx.intermediate
     proc = ir.proc
 
@@ -442,6 +444,11 @@ def clusterSingleBatches(ctx, sanity_plots=False, plot_widgets=None, plot_pos=0)
         # if reordering is turned off, return consecutive order
         iorig = np.arange(Nbatch)
         return iorig, None, None
+    
+    # intialize the random number generator with seed.
+    # Ensures a reproducible sequence when random spikes are drawn in initializeW2data
+    # Note that random number generator is re-intialized in learn
+    np.random.seed(params.seed)
 
     nPCs = params.nPCs
     Nfilt = ceil(probe.Nchan / 2)
@@ -472,6 +479,8 @@ def clusterSingleBatches(ctx, sanity_plots=False, plot_widgets=None, plot_pos=0)
 
     # return an array of closest channels for each channel
     iC = getClosestChannels(probe, params.sigmaMask, NchanNear)[0]
+    
+
 
     for ibatch in tqdm(range(nBatches), desc="Clustering spikes"):
         enough_spikes = False

pykilosort/learn.py
@@ -835,7 +835,7 @@ def learnAndSolve8b(ctx, sanity_plots=False, plot_widgets=None, plot_pos=None):
     ir = ctx.intermediate
     data_loader = ir.data_loader
 
-    iorig = ir.iorig
+    iorig = cp.asnumpy(ir.iorig)
 
     # TODO: move_to_config
     NrankPC = 6  # this one is the rank of the PCs, used to detect spikes with threshold crossings
@@ -873,11 +873,33 @@ def learnAndSolve8b(ctx, sanity_plots=False, plot_widgets=None, plot_pos=None):
     # find the closest NchanNear channels, and the masks for those channels
     iC, mask, C2C = getClosestChannels(probe, sigmaMask, NchanNear)
 
-    # batch order schedule is a random permutation of all batches
-    ischedule = np.random.permutation(nBatches)
-    i1 = np.arange(nBatches)
-
-    irounds = np.concatenate((ischedule, i1))
+    if params.reorder:
+        # for KS2.0 like sorting: run batches in first half of recording 
+        # From Marius' comments in the release version of KS2.0:
+        # we learn the templates by going back and forth through some of the data,
+        # in the order specified by iorig (determined by batch reordering).
+        # standard order -- learn templates from first half of data starting
+        # from midpoint, counting down to 1, and then returning.
+              
+        ihalf = np.ceil(nBatches/2).astype(int)
+        first_half = iorig[0:ihalf]
+        first_half_inv = first_half[::-1]   #invert order, start from midpoint
+        learn_order = np.concatenate((first_half_inv, first_half))
+        
+        # In the run templates iterations, start from the ihalf-1 batch and work
+        # to batch 0, then run ihalf to end
+        second_half = iorig[ihalf:]
+        run_order = np.concatenate((first_half_inv, second_half))        
+        irounds = np.concatenate((learn_order, run_order))
+        
+    else:
+        # For KS 2.5-like behavior, "learn" phase uses batches in random order
+        # "Run" phase uses batches in time order.
+        # Batch order schedule is a random permutation of all batches
+        ischedule = np.random.permutation(nBatches)
+        i1 = np.arange(nBatches)
+        irounds = np.concatenate((ischedule, i1))
+    
 
     niter = irounds.size
 
@@ -1240,6 +1262,20 @@ def learnAndSolve8b(ctx, sanity_plots=False, plot_widgets=None, plot_pos=None):
         free_gpu_memory()
 
     # Close the large array writers and save the JSON metadata files to disk.
+    
+    if params.reorder:
+        # For KS 2.0 like processing, want to sort the output arrays in order
+        # of similarity to avoid excess splitting in SplitAllClusters
+        # spikes get sorted by absolute time in rezToPhy
+        # python lexsort sorts the columns with the primary sort column last
+        st3c = np.concatenate(st3,axis=0)
+        sort_order_batch = np.argsort(run_order)
+        batch_ind = st3c[:,4].astype(int)
+        sort_order = sort_order_batch[batch_ind]
+        simOrder = np.lexsort((st3c[:,3],st3c[:,2],st3c[:,1],st3c[:,0],sort_order))
+        ir.simOrder = simOrder   #for accessing the feature arrays
+
+        
     fW.close()
     fWpc.close()
 
@@ -1278,6 +1314,7 @@ def learnAndSolve8b(ctx, sanity_plots=False, plot_widgets=None, plot_pos=None):
     # Save cluster times and IDs at this stage if requested
     if params.save_temp_files:
         np.save(ctx.context_path / 'temp_splits' / 'st3_learn.npy', ir.st3)
+        np.save(ctx.context_path / 'temp_splits' / 'simOrder_learn.npy', ir.simOrder)
 
     # # this whole next block is just done to compress the compressed templates
     # # we separately svd the time components of each template, and the spatial components
@@ -1325,6 +1362,7 @@ def learnAndSolve8b(ctx, sanity_plots=False, plot_widgets=None, plot_pos=None):
         U=ir.U,
         dWU=ir.dWU,
         mu=ir.mu,
+        simOrder=ir.simOrder
         # W_a=W_a,
         # W_b=W_b,
         # U_a=U_a,

pykilosort/main.py
@@ -49,6 +49,7 @@ def run(
     params = KilosortParams(**params or {})
     assert params
 
+    print(dat_path)
     raw_data = RawDataLoader(dat_path, **params.ephys_reader_args)
 
     # Get probe.
@@ -140,6 +141,8 @@ def run(
     ir.proc_path = ctx.path("proc", ".dat")
     if "preprocess" not in ctx.timer.keys():
         # Do not preprocess again if the proc.dat file already exists.
+        # set params.preprocessing_function = 'destriping' for ibl preprocessing
+        # set to 'kilosort2' for standard preprocessing 
         with ctx.time("preprocess"):
             if params.preprocessing_function == 'destriping':
                 destriping(ctx)
@@ -154,30 +157,41 @@ def run(
     ir.proc = np.memmap(ir.proc_path, dtype=raw_data.dtype, mode="r+", order="F")
     ir.data_loader = DataLoader(ir.proc_path, params.NT, probe.Nchan, params.scaleproc)
 
-    # -------------------------------------------------------------------------
-    # # Time-reordering as a function of drift.
-    # #
-    # # This function saves:
-    # #
-    # #       iorig, ccb0, ccbsort
-    # #
-    # if "iorig" not in ir:
-    #     with ctx.time("reorder"):
-    #         out = clusterSingleBatches(ctx)
-    #     ctx.save(**out)
-    # if stop_after == "reorder":
-    #     return ctx
-
-
-    if params.perform_drift_registration:
-        if "drift_correction" not in ctx.timer.keys():
-            with ctx.time("drift_correction"):
-                out = datashift2(ctx)
-            ctx.save(**out)
+     #-------------------------------------------------------------------------
+     # Time-reordering as a function of drift.
+     #
+     # This function saves:
+     #
+     #       iorig, ccb0, ccbsort
+     #
+     
+    if params.ks2_mode:
+        # means we're skipping drift registration
+        # set that parameter in case caller has not
+        print('running ClusterSingleBatches')
+        params.perform_drift_registration = False
+        params.reorder = 1
+        if "iorig" not in ir:
+             with ctx.time("reorder"):
+                 out = clusterSingleBatches(ctx)
+             ctx.save(**out)
+        if stop_after == "reorder":
+             return ctx       
+        print( repr(ctx.intermediate.iorig))
+        
     else:
-        ctx.intermediate.iorig = np.arange(ctx.intermediate.Nbatch)
-    if stop_after == "drift_correction":
-        return ctx
+        if params.perform_drift_registration:
+            if "drift_correction" not in ctx.timer.keys():
+                print('running drift correction')
+                with ctx.time("drift_correction"):
+                    out = datashift2(ctx)
+                ctx.save(**out)
+                print(repr(ctx.intermediate.iorig))
+        else:
+            ctx.intermediate.iorig = np.arange(ctx.intermediate.Nbatch)
+        if stop_after == "drift_correction":
+            return ctx
+    
     # -------------------------------------------------------------------------
     # Main tracking and template matching algorithm.
     #
@@ -205,6 +219,13 @@ def run(
     # Special care for cProj and cProjPC which are memmapped .dat files.
     ir.cProj = memmap_large_array(ctx.path("fW", ext=".dat")).T
     ir.cProjPC = memmap_large_array(ctx.path("fWpc", ext=".dat")).T  # transpose
+    
+    if params.ks2_mode:
+        # reorder by simBatches 
+        ir.st3 = ir.st3[ir.simOrder,:]
+        ir.cProj = ir.cProj[ir.simOrder,:]
+        ir.cProjPC = ir.cProjPC[ir.simOrder,:]
+
 
     # -------------------------------------------------------------------------
     # Final merges.

pykilosort/preprocess.py
@@ -13,12 +13,19 @@ logger = logging.getLogger(__name__)
 
 
 def get_filter_params(fs, fshigh=None, fslow=None):
-    if fslow and fslow < fs / 2:
+    if fslow and fslow > fs/2:    # can't apply a filter with freq > 1/2 sample rate
+        fslow = None
+        
+    if fslow and fshigh:
+        # valid low and high pass values, make a bandpass filter
         # butterworth filter with only 3 nodes (otherwise it's unstable for float32)
         return butter(3, (2 * fshigh / fs, 2 * fslow / fs), 'bandpass')
+    elif fslow:
+         # low pass butterworth
+        return butter(3, 2 * fslow / fs , 'low')        
     else:
-        # butterworth filter with only 3 nodes (otherwise it's unstable for float32)
-        return butter(3, fshigh / fs * 2, 'high')
+        # high pass butterworth
+        return butter(3, 2 * fshigh / fs, 'high')
 
 
 def gpufilter(buff, chanMap=None, fs=None, fslow=None, fshigh=None, car=True):
@@ -45,14 +52,18 @@ def gpufilter(buff, chanMap=None, fs=None, fslow=None, fshigh=None, car=True):
     if car:
         # subtract median across channels
         dataRAW = dataRAW - median(dataRAW, axis=1)[:, np.newaxis]
-
-    # set up the parameters of the filter
-    filter_params = get_filter_params(fs, fshigh=fshigh, fslow=fslow)
-
-    # next four lines should be equivalent to filtfilt (which cannot be
-    # used because it requires float64)
-    datr = lfilter(*filter_params, dataRAW, axis=0)  # causal forward filter
-    datr = lfilter(*filter_params, datr, axis=0, reverse=True)  # backward
+    
+    if (fshigh is not None) or (fslow is not None):
+        # set up the parameters of the filter
+        filter_params = get_filter_params(fs, fshigh=fshigh, fslow=fslow)
+    
+        # next four lines should be equivalent to filtfilt (which cannot be
+        # used because it requires float64)
+        datr = lfilter(*filter_params, dataRAW, axis=0)  # causal forward filter
+        datr = lfilter(*filter_params, datr, axis=0, reverse=True)  # backward
+    else:
+        datr = dataRAW
+        
     return datr
 
 
@@ -404,6 +415,10 @@ def preprocess(ctx):
 
     with open(ir.proc_path, 'wb') as fw:  # open for writing processed data
         for ibatch in tqdm(range(Nbatch), desc="Preprocessing"):
+            # JIC notes
+            # This routine is working with a phylib flatEphysReader object, which
+            # is addressed by time point (rather than indexing into the raw binary)
+            # This is the 'KS2.5-like' processed data file 
             # we'll create a binary file of batches of NT samples, which overlap consecutively
             # on params.ntbuff samples
             # in addition to that, we'll read another params.ntbuff samples from before and after,
@@ -429,7 +444,7 @@ def preprocess(ctx):
             # apply filters and median subtraction
             buff = cp.asarray(buff, dtype=np.float32)
 
-            datr = gpufilter(buff, chanMap=probe.chanMap, fs=fs, fshigh=fshigh, fslow=fslow)
+            datr = gpufilter(buff, chanMap=probe.chanMap, fs=fs, fshigh=fshigh, fslow=fslow, car=params.car)
 
             assert datr.flags.c_contiguous
 